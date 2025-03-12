c77ef8c134eae8b16192daa42ac66fe5b9b64ab1
Welokiheiakeaeloa Gosline-Niheu <115903567+weloniheu@users.noreply.github.com>
2025-03-11 17:27:56 -0700

    Update and rename navbar.ts to navbar.js

---
+++ navbar.js
@@ -0,0 +1,7 @@
+// navbar.js
+fetch('navbar.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('navbar').innerHTML = data;
+    })
+    .catch(error => console.error('Error loading navbar:', error));
